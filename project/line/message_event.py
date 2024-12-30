3dfcc06ad228d224557953e3283a04fe450571d7
mabyes1 <124116017+mabyes1@users.noreply.github.com>
2024-12-30 14:11:59 +0800

    Update message_event.py

--- project/line/message_event.py
+++ project/line/message_event.py
@@ -8,7 +8,7 @@ import config
 line_bot_api = LineBotApi(config.LINE_CHANNEL_ACCESS_TOKEN)
 firebase_url = config.FIREBASE_URL
 genai.configure(api_key = config.GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-pro')
 def handle_message(event) -> None:
  """Event - User sent message
 
