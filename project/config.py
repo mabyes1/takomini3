c9c6ad1476d5dbd7df5092b69dd9c3c27d12fac3
mabyes1 <124116017+mabyes1@users.noreply.github.com>
2024-12-30 14:00:11 +0800

    Update config.py

--- project/config.py
+++ project/config.py
@@ -1,10 +1,9 @@
 import os
-
 from dotenv import load_dotenv
-
 # Load environment variables
 load_dotenv()
-
 # LINE Bot 設定
 LINE_CHANNEL_SECRET = os.environ.get("LINE_CHANNEL_SECRET")
 LINE_CHANNEL_ACCESS_TOKEN = os.environ.get("LINE_CHANNEL_ACCESS_TOKEN")
+GOOGLE_API_KEY = os.environ.get('GEMINI_KEY')
+FIREBASE_URL = os.environ.get('FIREBASE_URL')
